d45ca8d1f8478ae00d394bc4e1976571253d7951
stojan.sljivic <stojan.sljivic@gmail.com>
2023-01-16 10:57:39 +0100

    Add exported function example.

--- packages/core/app/index.ts
+++ packages/core/app/index.ts
@@ -2,3 +2,8 @@ export class Foo {
   constructor(public readonly bar: string, public readonly qux: number) {
   }
 }
+
+
+export function fooFn(bar: string, qux: number) {
+  console.log('foo')
+}
--- packages/test/app/index.ts
+++ packages/test/app/index.ts
@@ -1,6 +1,7 @@
-import { Foo } from 'core'
+import { fooFn, Foo } from 'core'
 import { Kafka } from 'kafkajs'
 
 const foo = new Foo('foo', 123)
+fooFn('foo', 123)
 
 const kafka = new Kafka({ brokers: [] })
